29ef8f6295a63a32f4985df574d7cfacef7cc50c
Yo Yehudi <yo@intermine.org>
2019-05-02 10:08:52 +0100

    some refactor, mostly comments

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var fieldsToCheck = {
-  webProperties : {
-    prettyName : "Web Properties",
-    url : "web-properties"
+  webProperties: {
+    prettyName: "Web Properties",
+    url: "web-properties"
   }
 }
 
@@ -11,69 +11,86 @@ var mines, configs = {};
 
 //get all InterMines as soon as the DOM has loaded
 document.addEventListener("DOMContentLoaded", function() {
-  $.ajax("http://registry.intermine.org/service/instances").then(function(response) {
-    //storing mines globally
-    mines = response.instances;
+  $.ajax("http://registry.intermine.org/service/instances")
+    .then(function(response) {
+      //storing mines globally
+      mines = response.instances;
 
-    var minesList = document.getElementById("interMinesList");
+      var minesList = document.getElementById("interMinesList");
 
-    var headerRow =  minesList.querySelector("thead tr");
-    console.log(headerRow);
+      var headerRow = minesList.querySelector("thead tr");
 
-    headerRow.innerHTML = generateHeader();
+      headerRow.innerHTML = generateHeader();
 
-    //debug. remove when done.
-    console.log(mines);
+      //debug. remove when done.
+      console.log(mines);
 
-    //once everything is loaded, display a list of mines to the user
-    mines.map(function(mine) {
-      try {
-        minesList.append(mineNode(mine));
-        fetchConfig(mine);
-      } catch (e) {
-        console.log(e);
-      }
+      //once everything is loaded, display a list of mines to the user
+      mines.map(function(mine) {
+        try {
+          minesList.append(mineNode(mine));
+          fetchConfig(mine);
+        } catch (e) {
+          console.log(e);
+        }
+      });
     });
-  });
 });
 
+/**
+ * Generates HTML for each row associated with an InterMine
+ **/
 function mineNode(mine) {
   //Generate text for links to each mine.
   var mineNode = document.createElement("tr"),
     mineRow = "<td>" + mine.name + "</td>" +
-    "<td>" + mine.api_version + "</td>" +
-    "<td class='webProperties'>" + "loading config" + "</td>";
-  mineNode.organisms = mine.organisms;
+    "<td>" + mine.api_version + "</td>";
+  Object.keys(fieldsToCheck).map(function(fieldName) {
+    mineRow += "<td class='" + fieldName + "'>Loading</td>"
+  });
   mineNode.setAttribute("class", "mineEntry");
   mineNode.setAttribute("id", mine.namespace);
   mineNode.innerHTML = mineRow;
   return mineNode;
 }
 
+/**
+ * Fetches all configs we'd like to check from an InterMine - e.g.
+ * does the web properties endpoint work? Uses promises to handle the huge
+ * number of requests.
+ **/
 function fetchConfig(mine) {
   var thisMine = configs[mine.namespace] = {};
-  Object.keys(fieldsToCheck).map(function(fieldName){
+  Object.keys(fieldsToCheck).map(function(fieldName) {
     var field = fieldsToCheck[fieldName];
     thisMine[fieldName] = $.ajax(mine.url + "/service/" + field.url);
     thisMine[fieldName].done(function(response) {
-       thisMine[fieldName] = response;
-        updateCell(mine.namespace, fieldName);
-     });
-     thisMine[fieldName].fail(function(response) {
-        thisMine[fieldName] = false;
-        updateCell(mine.namespace, fieldName);
-     });
+      thisMine[fieldName] = response;
+      updateCell(mine.namespace, fieldName);
+    });
+    thisMine[fieldName].fail(function(response) {
+      thisMine[fieldName] = false;
+      updateCell(mine.namespace, fieldName);
+    });
   });
 }
 
+/**
+* Once a config is fetched and the promise is resolved or failed,
+show the value in the cell
+**/
 function updateCell(mine, cell) {
   var elem = document.getElementById(mine).getElementsByClassName(cell)[0];
   elem.innerHTML = configs[mine][cell] ? "Y" : "N"
 }
 
-function generateHeader(){
+/**
+ * Create the header row dynamically depending on the fields we have
+ * put in the settings.
+ **/
+function generateHeader() {
   var header = "<th>Mine Name</th> <th>API version</th>"
-  Object.keys(fieldsToCheck).map(function(field){
+  Object.keys(fieldsToCheck).map(function(field) {
     header += "<th>" + fieldsToCheck[field].prettyName + "</th>";
   });
   return header;
